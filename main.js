a1744cb4ec0f43c17ccc8a6ad49e6acb95f0a070
unknown <younes.sahar.ing@gmail.com>
2023-07-26 09:16:39 +0200

    CrawlPage works and removed getHTML

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const  {extractURL,extractURLsLoop,removeTrailingSlahes,getHTML} = require ("./crawl.js")
+
+async function crawlPage(url,urls){
+    console.log("Actively crawling: "+ url)
+    const response = await fetch(url)
+    const htmltext = await response.text()
+    //console.log(htmltext)
+    extractURL(htmltext,url,urls)
+    console.log(urls)
+    return urls
+}
+
 function main(){
 if (process.argv.length<3){
     console.log("No website provided")
@@ -7,8 +18,9 @@ if (process.argv.length<3){
 if (process.argv.length>3){
     console.log("Too many arguments provided")
     process.exit(1)
-}        
-var baseURLObj
+}
+
+let baseURLObj
 try {
     baseURLObj = new URL(process.argv[2])
 }catch(err){
@@ -17,26 +29,8 @@ try {
 }
 //console.log(baseURLObj.href)
 const baseURL = removeTrailingSlahes(baseURLObj.href)
-const urlsObj = {}
-urlsObj[baseURL] = 0
-console.log(urlsObj)
-crawlPage()
-//----------------function definition:-------------
-function crawlPage(urlsObj){
-
-    const url=baseURL // url should be the key from the object loop
-    console.log("Actively crawling: "+ url)
-    
-
-    
-    const htmltext = getHTML(url,urlsObj)
-    .then((htmlOutPut)=>{ 
-        console.log(urls)
-        const urls = extractURL (htmlOutPut,baseURL,urls)
-        return urls
-    })
+crawlPage(baseURL,urls={})
     return   
-}
 }
 
 main()
